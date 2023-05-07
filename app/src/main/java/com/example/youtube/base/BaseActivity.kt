007154d2499aacbe1d7c90d3ade4d2af0932a98e
Nigora <esenbekovanigora@gmail.com>
2023-05-08 00:40:41 +0600

    internet

--- app/src/main/java/com/example/youtube/base/BaseActivity.kt
+++ app/src/main/java/com/example/youtube/base/BaseActivity.kt
@@ -1,10 +1,17 @@
 package com.example.youtube.base
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity<VB : ViewBinding, VM : BaseViewModel> : AppCompatActivity() {
+abstract class BaseActivity<VB : ViewBinding, VM : BaseViewModel> (): AppCompatActivity() {
+
+    abstract fun isInternetAvailable(): Boolean
 
     protected lateinit var binding: VB
     protected abstract fun inflateViewBinding(): VB
@@ -25,4 +32,5 @@ abstract class BaseActivity<VB : ViewBinding, VM : BaseViewModel> : AppCompatAct
     open fun initListener() {} // Все наши клики
     open fun initViewModel() {} // Все обзерверы нащего viewModel\'a
     open fun isConnection() {} // Проверка на подключение к интернету
+
 }
